f79b1976674a04136f5ec6e0d6029c71ba883eb2
Robert Kitzing <r.kitzing@hotmail.de>
2018-02-20 00:12:07 +0100

    detail_row

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,3 +1,4 @@
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs/Subscription';
 import { SeasonService } from '@app/service/season.service';
@@ -8,28 +9,54 @@ import { MatTableDataSource, MatSort, Sort } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 import { DataSource } from '@angular/cdk/collections';
 import { Logger } from '@app/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 const log = new Logger('Table');
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class TableComponent implements OnInit, OnDestroy {
 
   version: string = environment.version;
-  displayedColumns = ['number',
-                      'team_id',
-                      'matches',
-                      'wins',
-                      'draws',
-                      'losses',
-                      'scored_goals',
-                      'conceded_goals',
-                      'goals_diff',
-                      'points'];
-  rankingDataSource: MatTableDataSource<Position[]>;
+
+  allColumns = ['number',
+                'team_id',
+                'matches',
+                'wins',
+                'draws',
+                'losses',
+                'scored_goals',
+                'conceded_goals',
+                'goals_diff',
+                'points'];
+
+  xsColumns = [ 'number',
+                'team_id',
+                'matches',
+                'points'];
+
+  smColumns = [ 'number',
+                'team_id',
+                'matches',
+                'scored_goals',
+                'conceded_goals',
+                'points'];
+
+  displayedColumns: string[];
+
+  expandedElement: any;
+
+  rankingDataSource: RankingDataSource;
   seasons: Season[] = new Array<Season>();
   seasonsSub: Subscription = new Subscription();
   season: Season;
@@ -38,16 +65,29 @@ export class TableComponent implements OnInit, OnDestroy {
   isLoadingRanking: boolean;
 
   @ViewChild(MatSort) sort: MatSort;
-  public state: string = '';
+
   constructor(private apiClient: Client,
               public seasonService: SeasonService,
               public media: ObservableMedia) {
                 media.asObservable()
                 .subscribe((change: MediaChange) => {
-                  this.state = change.mqAlias;
-                  log.debug(this.state);
+                  switch (change.mqAlias) {
+                    case 'xs': {
+                      this.displayedColumns = this.xsColumns;
+                      break;
+                    }
+                    case 'sm': {
+                      this.displayedColumns = this.smColumns;
+                      break;
+                    }
+                    default : {
+                      this.displayedColumns = this.allColumns;
+                    }
+                  }
                 });
               }
+
+  isExpansionDetailRow = (i: any, row: any) => row.hasOwnProperty('detailRow');
 
   async ngOnInit() {
     this.season = this.seasonService.getSelectedSeason();
@@ -84,8 +124,7 @@ export class TableComponent implements OnInit, OnDestroy {
     this.isLoadingRanking = true;
     this.apiClient.ranking(this.season.id).subscribe(
       (ranking: any) => {
-        log.debug(ranking);
-        this.rankingDataSource = new MatTableDataSource(ranking.positions);
+        this.rankingDataSource = new RankingDataSource(ranking.positions);
         this.rankingDataSource.sort = this.sort;
       },
       (error: any) => {
@@ -99,5 +138,11 @@ export class TableComponent implements OnInit, OnDestroy {
 
   seasonCompare(c1: Season, c2: Season) {
     return c1 && c2 && c1.id === c2.id;
+  }
+}
+
+class RankingDataSource extends MatTableDataSource<Position> {
+  constructor(ranking: Position[]) {
+    super(ranking);
   }
 }
