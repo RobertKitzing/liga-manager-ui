69c8b0ffa3cb0625e0c83dfc28ceefd4c0a8dce6
Robert Kitzing <r.kitzing@arcor.de>
2018-03-19 20:17:05 +0100

    logos auf wildeLiga

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,9 @@ import { QuoteService } from './quote.service';
 })
 export class HomeComponent implements OnInit {
 
-  quote: string;
-  isLoading: boolean;
-
-  constructor(private quoteService: QuoteService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.isLoading = true;
-    this.quoteService.getRandomQuote({ category: 'dev' })
-      .pipe(finalize(() => { this.isLoading = false; }))
-      .subscribe((quote: string) => { this.quote = quote; });
   }
 
 }
