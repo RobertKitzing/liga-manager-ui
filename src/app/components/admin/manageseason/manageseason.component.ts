4cb15f9607cce9980ca002ad31c6ec888a7c6549
Robert Kitzing <r.kitzing@hotmail.de>
2018-07-21 16:38:17 +0200

    startSeaonrdy

--- src/app/components/admin/manageseason/manageseason.component.ts
+++ src/app/components/admin/manageseason/manageseason.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SeasonService } from '../../../services/season.service';
-import { Season, SeasonState, Team, Client } from 'src/api';
+import { Season, SeasonState, Team, Client, CreateSeasonBody, CreateMatchesBody, Match } from 'src/api';
 import { MatSelectChange } from '@angular/material';
+import { MatchService } from '../../../services/match.service';
 
 @Component({
   selector: 'app-manageseason',
@@ -13,20 +14,39 @@ export class ManageseasonComponent implements OnInit {
   seasons: Season[];
   teamsInSeason: Team[];
   allTeams: Team[];
-
+  matchesInSeason: Match[];
   manageSeason: Season;
+  matchDayCounter: number[];
 
   constructor(
     public seasonService: SeasonService,
+    private matchService: MatchService,
     private apiClient: Client
   ) { }
 
   async ngOnInit() {
+    this.loadAllSeasonInPrep();
+    this.loadAllTeams();
+  }
+
+  async loadAllSeasonInPrep() {
     this.seasons = await this.seasonService.loadSeasons(SeasonState.Preparation);
-    console.log(this.seasons);
+  }
+
+  loadAllTeams() {
     this.apiClient.getAllTeams().subscribe(
       (teams) => {
         this.allTeams = teams.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+      }
+    );
+  }
+
+  addNewSeason(seasonName: string) {
+    const createSeasonBody = new CreateSeasonBody();
+    createSeasonBody.name = seasonName;
+    this.apiClient.createSeason(createSeasonBody).subscribe(
+      (t) => {
+        this.loadAllSeasonInPrep();
       }
     );
   }
@@ -52,5 +72,28 @@ export class ManageseasonComponent implements OnInit {
         }
       );
     }
+  }
+
+  createMatches() {
+    const body = new CreateMatchesBody();
+    body.start_at = new Date();
+    this.apiClient.createMatches(this.manageSeason.id, body).subscribe(
+      async (m) => {
+        this.apiClient.getSeason(this.manageSeason.id).subscribe(
+          async (season) => {
+            this.matchDayCounter = Array.from(new Array(season.match_day_count), (val, index) => index + 1);
+            await this.getMatchesInSeason(1);
+          }
+        );
+      }
+    );
+  }
+
+  async getMatchesInSeason(matchDay) {
+    this.matchesInSeason = await this.matchService.getMatchesInSeason(this.manageSeason.id, matchDay);
+  }
+
+  startSeason() {
+    this.apiClient.startSeason(this.manageSeason.id).toPromise();
   }
 }
