3e4343c25f479706517c1755df9e38a44b58d9c3
Robert Kitzing <r.kitzing@hotmail.de>
2018-10-20 01:20:44 +0200

    commit id removed from changelog

--- app/createchangelog.js
+++ app/createchangelog.js
@@ -8,7 +8,7 @@ const { exec } = require('child_process');
 var fs = require('fs');
 const fastLaneDir = "./metadata/de-DE/changelogs";
 
-exec("git log --oneline $(git describe --tags --abbrev=0 @^)..@",
+exec('git log --oneline $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%s"',
     function (error, out) {
         if (error instanceof Error)
             throw error;
