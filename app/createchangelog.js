2322cc78be5324524b93601884aabaf9db37531a
Robert Kitzing <r.kitzing@hotmail.de>
2018-10-20 11:27:54 +0200

    fixed codetyle issues

--- app/createchangelog.js
+++ app/createchangelog.js
@@ -1,20 +1,21 @@
 const optionDefinitions = [
-    { name: 'versionCode', alias: 'v', type: String },
-    { name: 'tagName', alias: 't', type: String }
-]
-const commandLineArgs = require('command-line-args')
-const options = commandLineArgs(optionDefinitions)
-const { exec } = require('child_process');
-var fs = require('fs');
+    { name: "versionCode", alias: "v", type: String },
+    { name: "tagName", alias: "t", type: String }
+];
+const commandLineArgs = require("command-line-args");
+const options = commandLineArgs(optionDefinitions);
+const { exec } = require("child_process");
+var fs = require("fs");
 const fastLaneDir = "./metadata/de-DE/changelogs";
 
-exec('git log --oneline $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%s"',
+exec("git log --oneline $(git describe --tags --abbrev=0 @^)..@ --pretty=format:\"%s\"",
     function (error, out) {
-        if (error instanceof Error)
+        if (error instanceof Error) {
             throw error;
+        }
         fs.writeFile(fastLaneDir + "/" + options.versionCode + ".txt", out, function (err) {
             if (err) {
-                return console.log(err);
+                console.error(err);
             }
 
             console.log("The file was saved!");
