dfe1c63cd55a26c062403e9a280dabe64380479e
Robert Kitzing <r.kitzing@hotmail.de>
2018-10-21 23:41:05 +0200

    added browsers to travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [
-      "Chrome",
+      "ChromeHeadless",
       "Firefox"
     ],
     singleRun: false
