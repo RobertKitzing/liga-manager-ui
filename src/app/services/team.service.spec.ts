da252a4eb24877f300c61755c6243d11f034d613
Robert Kitzing <r.kitzing@hotmail.de>
2018-10-24 08:35:42 +0200

    added test in team.service

--- src/app/services/team.service.spec.ts
+++ src/app/services/team.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { TeamService } from './team.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Team } from '../../api';
 
 describe('TeamService', () => {
   beforeEach(() => {
@@ -15,5 +16,13 @@ describe('TeamService', () => {
 
   it('should be created', inject([TeamService], (service: TeamService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should save Team[] to localStorage', inject([TeamService], (service: TeamService) => {
+    const testTeams: Team[] = new Array<Team>();
+    testTeams.push(new Team({ name: 'TestTeam' }));
+    const localStorageSpy = spyOn(Object.getPrototypeOf(localStorage), 'setItem');
+    service.teams = testTeams;
+    expect(localStorageSpy).toHaveBeenCalledWith('TEAMS', JSON.stringify(testTeams));
   }));
 });
