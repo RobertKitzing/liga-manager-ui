cd81479025655bc6dbdf4c2b27c651c5143acc9d
Travis CI <r.kitzing@hotmail.de>
2019-03-24 18:45:05 +0100

    refresh button added to table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -4,6 +4,9 @@ import { Ranking, RankingGQL } from 'src/api/graphql';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { I18Service } from '../../services/i18.service';
+import { MatSnackBar } from '@angular/material';
+import { SnackbarComponent } from 'src/app/components/shared/snackbar/snackbar.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-table',
@@ -13,11 +16,15 @@ import { I18Service } from '../../services/i18.service';
 export class TableComponent implements OnInit {
 
   rankingQGL: Observable<Ranking.Ranking>;
+  error: boolean;
 
   constructor(
     public seasonService: SeasonService,
     public i18Service: I18Service,
-    private ranking: RankingGQL) {
+    public snackBar: MatSnackBar,
+    private ranking: RankingGQL,
+    private translateService: TranslateService
+  ) {
   }
 
   ngOnInit() {
@@ -27,8 +34,37 @@ export class TableComponent implements OnInit {
   }
 
   getRanking() {
-    this.rankingQGL = this.ranking.watch({id: this.seasonService.currentSeason.getValue().id}).valueChanges.pipe(
-      map(({data}) => data.season.ranking)
+    this.error = false;
+    this.rankingQGL = this.ranking.watch({ id: this.seasonService.currentSeason.getValue().id }).valueChanges.pipe(
+      map(
+        (result) => {
+          if (result.errors) {
+            console.error(result.errors);
+            this.showErrorSnackBar();
+            return null;
+          } else {
+            if (!result.data.season) {
+              this.showErrorSnackBar();
+            }
+            return result.data.season.ranking;
+          }
+        }
+      )
     );
+  }
+
+  refresh() {
+    this.ranking.watch({ id: this.seasonService.currentSeason.getValue().id }).refetch();
+  }
+
+  showErrorSnackBar(errorCode?: number) {
+    this.error = true;
+    this.snackBar.openFromComponent(SnackbarComponent, {
+      data: {
+        message: this.translateService.instant('COMMON_ERROR'),
+        duration: 60000
+      },
+      panelClass: ['alert', 'alert-danger'],
+    });
   }
 }
