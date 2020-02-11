05832b0b1a3098c3be75463a793b57993b490150
Travis CI <r.kitzing@hotmail.de>
2020-02-12 00:13:01 +0100

    seperate day/time input in editmatch component

--- src/app/components/shared/editmatch/editmatch.time.component.ts
+++ src/app/components/shared/editmatch/editmatch.time.component.ts
@@ -1,45 +1,60 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DateTimeAdapter } from 'ng-pick-datetime';
 import { TranslateService } from '@ngx-translate/core';
 import { I18Service } from '../../../services/i18.service';
 import { MatchService } from '../../../services/match.service';
 import { Match } from 'src/api/graphql';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-editmatch.time',
   templateUrl: './editmatch.time.component.html',
-  styleUrls: ['./editmatch.time.component.css']
+  styleUrls: ['./editmatch.time.component.css'],
 })
 export class EditmatchTimeComponent implements OnInit {
+
+  kickoffDate: Date;
+  kickoffTime: string;
 
   matchKickoff: Date;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public match: Match.Fragment,
-    dateTimeAdapter: DateTimeAdapter<any>,
     private translateService: TranslateService,
     public i18Service: I18Service,
     private dialogRef: MatDialogRef<EditmatchTimeComponent>,
-    private matchService: MatchService
+    private matchService: MatchService,
+    private dateAdapter: DateAdapter<any>
   ) {
-    dateTimeAdapter.setLocale(this.i18Service.currentLang);
     this.translateService.onLangChange.subscribe(
       (lang) => {
-        dateTimeAdapter.setLocale(lang);
+        this.dateAdapter.setLocale(lang);
       }
     );
+    this.dateAdapter.setLocale(this.translateService.currentLang);
   }
 
   ngOnInit() {
   }
 
-  kickoffSet(event: any) {
-    this.matchKickoff = event.value;
+  kickoffChanged() {
+    try {
+      console.log(this.kickoffTime);
+      if (this.kickoffTime) {
+        this.kickoffDate.setHours(+this.kickoffTime.split(':')[0]);
+        this.kickoffDate.setMinutes(+this.kickoffTime.split(':')[1]);
+        this.matchKickoff = new Date(this.kickoffDate.getTime());
+      } else {
+        delete this.matchKickoff;
+      }
+    } catch (error) {
+      delete this.matchKickoff;
+    }
   }
 
   onSaveClicked() {
-    this.matchService.scheduleMatch(this.match.id, this.matchKickoff)
+    if (this.matchKickoff) {
+      this.matchService.scheduleMatch(this.match.id, this.matchKickoff)
       .then(
         () => {
           this.dialogRef.close(true);
@@ -50,5 +65,6 @@ export class EditmatchTimeComponent implements OnInit {
           console.error(error);
         }
       );
+    }
   }
 }
