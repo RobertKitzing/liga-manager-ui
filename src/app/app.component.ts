4839e6d122d8fe42797691cb4cf8b8c1bda66e5d
Robert Kitzing <r.kitzing@hotmail.de>
2022-12-06 00:38:16 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lima-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'liga-manager-ui-v3';
+}
