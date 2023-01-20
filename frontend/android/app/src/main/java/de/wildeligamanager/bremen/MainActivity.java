a49cd1c3e7a8ca5684e2357295cc6af9c51f8e1a
Robert Kitzing <r.kitzing@hotmail.de>
2023-01-21 00:56:53 +0100

    added Capacitor

---
+++ frontend/android/app/src/main/java/de/wildeligamanager/bremen/MainActivity.java
@@ -0,0 +1,5 @@
+package de.wildeligamanager.bremen;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.wildeligamanager.bremen',
+  appName: 'liga-manager-ui',
+  webDir: 'dist/liga-manager-ui',
+  bundledWebRuntime: false
+};
+
+export default config;
