ad6505b0afeca692c809563d0dfda6bfdd8d03dc
Robert Kitzing <r.kitzing@hotmail.de>
2023-01-23 21:58:05 +0100

    wip

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,7 +4,12 @@ const config: CapacitorConfig = {
   appId: 'de.test.wildeligamanager.bremen',
   appName: 'liga-manager-ui',
   webDir: 'dist/liga-manager-ui',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
