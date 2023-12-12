80fd92fda9bb86b64c12870b561fe99ddb009bc9
Robert Kitzing <r.kitzing@hotmail.de>
2023-12-12 17:26:49 +0100

    added projectId

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "dnpc8n",
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
--- frontend/cypress/e2e/0-schedule/schedule.cy.ts
+++ frontend/cypress/e2e/0-schedule/schedule.cy.ts
@@ -3,7 +3,6 @@ import enGB from '@lima-i18n/en-GB.json';
 describe('Schedule', () => {
     it('Should go to Schedule', () => {
         cy.visit(`/`)
-        cy.contains(enGB['NAVIGATION.SCHEDULE']).click();
         cy.get('lima-schedule').should('exist')
         cy.contains(enGB['PLACEHOLDER.SELECT_SEASON']).click().get('mat-option').first().click();
         cy.get('h3').should('exist');
