7c54e4d471e9e5bc52d44c09218fc1f88edef88f
Robert Kitzing <r.kitzing@hotmail.de>
2023-12-12 01:55:27 +0100

    added translations to tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   viewportHeight: 1080,
   e2e: {
     baseUrl: 'http://localhost:4200',
+    
   },
-
 })
--- frontend/cypress/e2e/1-login/login.cy.ts
+++ frontend/cypress/e2e/1-login/login.cy.ts
@@ -1,11 +1,11 @@
-import { APP_ROUTES } from '@lima/app.routes.enum'
+import enGB from '@lima-i18n/en-GB.json';
 
 describe('Login', () => {
     it('Should Login', () => {
         cy.visit(`/`)
-        cy.get('#login').click();
-        cy.get('#username').type('admin@admin');
-        cy.get('#password').type('admin123');
-        cy.get('#execute-login').click();
+        cy.contains(enGB['BUTTON.LOGIN']).click();
+        cy.get('[data-cy="username"]').type('admin@admin');
+        cy.get('[data-cy="password"]').type('admin123');
+        cy.get('[data-cy="login-button"]').click();
     })
 })
