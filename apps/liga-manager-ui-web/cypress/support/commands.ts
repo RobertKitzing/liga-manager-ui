8193f98261ff78c7d2f8f00609c55f1dcffd53d4
Robert Kitzing <r.kitzing@hotmail.de>
2025-07-02 09:48:11 +0200

    wip

--- apps/liga-manager-ui-web/cypress/support/commands.ts
+++ apps/liga-manager-ui-web/cypress/support/commands.ts
@@ -1,3 +1,38 @@
+import { Base64 } from 'js-base64';
+
 Cypress.Commands.add('getBySel', (selector, ...args) => {
     return cy.get(`[data-cy="${selector}"]`, ...args)
 })
+
+const login = (username: string, password: string) => {
+    const query = `
+    {
+        authenticatedUser {
+          id
+        }
+    }
+    `
+    const body = {
+        operationName: 'AuthenticatedUser',
+        query,
+        variables: {},
+    }
+    cy.request({
+        method: 'POST',
+        url: '/api/graphql',
+        body,
+        headers: {
+            'Authorization': `Basic ${Base64.encode(
+                username.toLowerCase() +
+                    ':' +
+                    password,
+            )}`,
+        },
+    }).then(
+        (res) => {
+            localStorage.setItem('ngx-webstorage|access_token', `"${res.headers['x-token']}"`)
+        },
+    )
+}
+
+Cypress.Commands.add('login', login )
