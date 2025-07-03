0576fa4a89e097fd95961f18ef598a962a0fc5ff
Robert Kitzing <r.kitzing@hotmail.de>
2025-07-03 18:51:24 +0200

    wip: create season

--- apps/liga-manager-ui-web/src/app/admin/manage-seasons/manage-matchdays/manage-matchdays.component.ts
+++ apps/liga-manager-ui-web/src/app/admin/manage-seasons/manage-matchdays/manage-matchdays.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ManageSeasonBaseComponent } from '../manage-season.base.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
+import { DatePeriod } from '@liga-manager-api/graphql';
+import dayjs from 'dayjs';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'lima-manage-matchdays',
@@ -15,6 +18,7 @@ import { MatInputModule } from '@angular/material/input';
         MatFormFieldModule,
         MatDatepickerModule,
         MatInputModule,
+        DatePipe,
     ],
     templateUrl: './manage-matchdays.component.html',
 })
@@ -22,8 +26,33 @@ export class ManageMatchdaysComponent extends ManageSeasonBaseComponent {
 
     seasonStartDate = new FormControl<Date | undefined>(undefined);
 
+    secondHalf = new FormControl(false);
+
+    betweenMatchDaysOffset = new FormControl<number>(7, { nonNullable: true });
+
+    fromToOffset = new FormControl<number>(2, { nonNullable: true });
+
+    matchDays = signal<DatePeriod[]>([]);
+
     createMatchDays() {
-        console.log(this.season);
+        if (this.season?.teams) {
+            this.matchDays.set([]);
+            let length = this.season.teams.length;
+            if (length % 2 !== 0) {
+                length += 1;
+            }
+            if (this.secondHalf.value) {
+                length = (length * 2) - 1;
+            }
+            for (let i = 0; i < length - 1; i++) {
+
+                const dp = {} as DatePeriod;
+
+                dp.from = dayjs(this.seasonStartDate.value).add( i * this.betweenMatchDaysOffset.value, 'day').toDate();
+                dp.to = dayjs(dp.from).add(this.fromToOffset.value, 'day').toDate();
+                this.matchDays().push(dp);
+            }
+        }
     }
 
 }
