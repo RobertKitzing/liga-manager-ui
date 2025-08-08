810f40b6a0d43195dd2893ee3854215dcc989157
Robert Kitzing <r.kitzing@hotmail.de>
2025-08-09 00:17:33 +0200

    wip: styling calendar

--- apps/liga-manager-ui-web/src/app/calendar/calendar.component.ts
+++ apps/liga-manager-ui-web/src/app/calendar/calendar.component.ts
@@ -11,16 +11,29 @@ import listPlugin from '@fullcalendar/list';
 import { CalendarService, I18nService } from '@liga-manager-ui/services';
 import { Subject, switchMap, tap } from 'rxjs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FormControl } from '@angular/forms';
-import { TranslateService } from '@ngx-translate/core';
-import { MatchComponent } from '@liga-manager-ui/components';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatchComponent, SeasonChooserComponent } from '@liga-manager-ui/components';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'lima-calendar',
     templateUrl: './calendar.component.html',
     styles: [],
     imports: [
-        AsyncPipe, FullCalendarModule, MatToolbarModule, MatchComponent,
+        AsyncPipe,
+        FullCalendarModule,
+        MatToolbarModule,
+        MatchComponent,
+        SeasonChooserComponent,
+        MatIcon,
+        MatButtonModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        TranslateModule,
     ],
     standalone: true,
 })
@@ -34,17 +47,27 @@ export class CalendarComponent implements OnInit, AfterViewInit {
 
     selectedSeasonFC = new FormControl();
 
+    selectedView = new FormControl('list1Year', { nonNullable: true });
+
     eventTrigger = new Subject<CalendarQueryVariables>();
 
     calendarOptions: CalendarOptions = {
         headerToolbar: {
-            start: 'title',
-            center: 'dayGridMonth,dayGridWeek list1Year',
-            end: 'today prev,next',
+            start: '',
+            center: 'title',
+            end: '',
         },
-        initialView: 'list1Year',
+        initialView: this.selectedView.value,
         plugins: [dayGridPlugin, listPlugin],
         views: {
+            list1Week: {
+                type: 'list',
+                duration: { week: 1 },
+            },
+            list1Month: {
+                type: 'list',
+                duration: { month: 1 },
+            },
             list1Year: {
                 type: 'list',
                 duration: { year: 1 },
@@ -61,7 +84,6 @@ export class CalendarComponent implements OnInit, AfterViewInit {
         switchMap((params) =>
             this.calendarService.getCalendarEvents(params).pipe(
                 tap((events) => {
-                    console.log(events);
                     this.calendarOptions.events = events;
                 }),
             ),
@@ -80,6 +102,14 @@ export class CalendarComponent implements OnInit, AfterViewInit {
         this.calendarService.reloadEvents();
         this.calendarOptions.locale = this.i18nService.currentLang;
         this.translateService.onLangChange.subscribe((lang) => this.calendarOptions.locale = lang.lang);
+        this.selectedView.valueChanges.pipe(
+            takeUntilDestroyed(this.destroyRef),
+        ).subscribe(
+            (view) => {
+                console.log(view)
+                this.calendarApi.changeView(view)
+            },
+        );
     }
 
     ngAfterViewInit() {
@@ -96,6 +126,18 @@ export class CalendarComponent implements OnInit, AfterViewInit {
             min_date: this.calendarApi.view.activeStart,
             max_date: this.calendarApi.view.activeEnd,
         });
+    }
+
+    prev() {
+        this.calendarApi.prev();
+    }
+
+    next() {
+        this.calendarApi.next();
+    }
+
+    today() {
+        this.calendarApi.today();
     }
 
 }
