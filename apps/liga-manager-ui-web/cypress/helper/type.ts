fd64b85ec8226755d877e3782a2f4a6673f4674f
Robert Kitzing <r.kitzing@hotmail.de>
2025-10-10 16:59:11 +0200

    chore: fixed e2e

--- apps/liga-manager-ui-web/cypress/helper/type.ts
+++ apps/liga-manager-ui-web/cypress/helper/type.ts
@@ -2,6 +2,6 @@ import { CySelectors } from '@liga-manager-ui/directives';
 
 export function type(selector: CySelectors, value: string) {
     cy.getBySel(selector).click({ force: true });
-    cy.getBySel(selector).clear();
+    cy.getBySel(selector).clear({ force: true });
     cy.getBySel(selector).type(value);
 }
